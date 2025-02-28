cd44684a46d137d7d3eae49e75c9873ede98446c
Rupam Sarkar <161940237+Rupamsarkar123@users.noreply.github.com>
2025-02-28 06:02:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-const port = Number(process.env.PORT) || 5174;
-console.log(`Server running on port ${port}`);
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port, 
+    port: Number(process.env.PORT) || 10000,
     host: "0.0.0.0",
+    strictPort: true,
+    allowedHosts: ["chatai-1-2enq.onrender.com"], // Add your Render domain
   },
 });
