3c5753a3b169a1518c7a21b430fa4c21e00668a4
Rupam Sarkar <161940237+Rupamsarkar123@users.noreply.github.com>
2025-02-28 05:30:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc"; // Use SWC instead of Babel
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: process.env.PORT || 5174,
     host: "0.0.0.0",
-  },
-  optimizeDeps: {
-    exclude: ["antd"], // Exclude Ant Design from dependency optimization
   },
 });
