e96a321420fe5c364ac0828e36d583f3df879dd5
Rupam Sarkar <161940237+Rupamsarkar123@users.noreply.github.com>
2025-03-04 14:17:24 +0530

    Update vite.config.js added frontend url

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,13 @@ import react from "@vitejs/plugin-react"; // Use the default React plugin
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: Number(process.env.PORT) || 5174, // Use a dynamic port
+    port: Number(process.env.PORT) || 5174,
     host: "0.0.0.0",
     strictPort: true,
-    allowedHosts: ["chatai-1-2enq.onrender.com", "localhost"], // Allow Render + local testing
+    allowedHosts: [
+      "chatai-1-il4g.onrender.com", //  correct frontend Render URL
+      "localhost",
+    ],
   },
 });
+
